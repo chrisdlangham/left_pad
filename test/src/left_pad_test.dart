c2265d30791e7c9893715fe1e8f2de61078eecc3
Chris Langham <chrislangham@rightnow.org>
2023-08-13 22:42:49 -0500

    style: run formatter

--- test/src/left_pad_test.dart
+++ test/src/left_pad_test.dart
@@ -2,14 +2,20 @@ import 'package:left_pad/left_pad.dart';
 import 'package:test/test.dart';
 
 void main() {
-    test('leftPad', () {
-      expect(leftPad('foo', targetLength: 5), '  foo');
-      expect(leftPad('foobar', targetLength: 6), 'foobar');
-    });
+  test('leftPad', () {
+    expect(leftPad('foo', targetLength: 5), '  foo');
+    expect(leftPad('foobar', targetLength: 6), 'foobar');
+  });
 
-    test('left pad with pad string', () {
-      expect(leftPad('foo', targetLength: 5, padString: '*'), '**foo');
-      expect(() => leftPad('foo', targetLength: 5, padString: ''), throwsArgumentError);
-      expect(() => leftPad('foo', targetLength: 5, padString: '_-'), throwsArgumentError);
-    });
+  test('left pad with pad string', () {
+    expect(leftPad('foo', targetLength: 5, padString: '*'), '**foo');
+    expect(
+      () => leftPad('foo', targetLength: 5, padString: ''),
+      throwsArgumentError,
+    );
+    expect(
+      () => leftPad('foo', targetLength: 5, padString: '_-'),
+      throwsArgumentError,
+    );
+  });
 }
